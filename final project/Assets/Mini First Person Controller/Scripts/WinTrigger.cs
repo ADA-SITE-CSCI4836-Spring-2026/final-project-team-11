46801a0b2f285fa2ddd15e860af3a0f5fc81e70a
suelheybatova1 <123533417+suelheybatova1@users.noreply.github.com>
2026-05-02 16:40:06 +0400

    update

---
+++ final project/Assets/Mini First Person Controller/Scripts/WinTrigger.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("YOU WIN");
+
+            Time.timeScale = 0f;
+        }
+    }
+}
